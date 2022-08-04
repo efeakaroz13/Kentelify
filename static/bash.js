b6c26be0f9f24b70f3f1acfcd679d499216f73f6
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-05 00:36:26 +0300

    virtual folder and number update

--- static/bash.js
+++ static/bash.js
@@ -25,7 +25,9 @@ document.addEventListener('keydown', (e) => {
 
     
     if(letter == undefined){
-        
+        if(e.code == "Minus"){
+            document.getElementById("query").innerHTML =document.getElementById("query").innerHTML+"-"
+        }
         if(e.code == "ShiftRight"){
             letterlist.length = 0;
 
@@ -63,8 +65,23 @@ document.addEventListener('keydown', (e) => {
             if(input.trim() == "clear" || input.trim() == "cls" ){
                 document.getElementById("logs").innerHTML = ""
             }
-            if(input.trim().split("alert(")[1] !=undefined){
+            else if(input.trim().split("alert(")[1] !=undefined){
                 alert(input.trim().split("alert(")[1].replace(")",""))
+            }else if(input.trim().split("--help")[1] !=undefined){
+                document.getElementById("logs").innerHTML = document.getElementById("logs").innerHTML+"<p>Here is list of commands:<br>clear<br>alert(your_text)<br> --about</p>"
+            }
+            else if(input.trim().split("--about")[1] !=undefined){
+                document.getElementById("logs").innerHTML = document.getElementById("logs").innerHTML+"<p>Kentelify bash emulator(JS) <br>Author:Efe Akaröz 4th Agust 2022</p>"
+            }
+            else if(input.trim()=="ls" ||input.trim()=="dir"){
+                document.getElementById("logs").innerHTML = document.getElementById("logs").innerHTML+"<p>music_download_script.py</p>"
+            }
+            else if(input.trim()==""){
+                null
+            }
+        
+            else{
+                document.getElementById("logs").innerHTML = document.getElementById("logs").innerHTML+"<p>Unknonw command '"+input+"', type --help for seeing commands</p>"
             }
         }
 
@@ -103,6 +120,62 @@ document.addEventListener('keydown', (e) => {
                 document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+")"
             }else{
                 document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"9"
+            }
+        }
+        if (digit == "1") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"!"
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"1"
+            }
+        }
+        if (digit == "2") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"'"
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"2"
+            }
+        }
+        if (digit == "3") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"#"
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"3"
+            }
+        }
+        if (digit == "4") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"+"
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"4"
+            }
+        }
+        if (digit == "5") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"%"
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"5"
+            }
+        }
+        if (digit == "6") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"6"
+            }
+        }
+        if (digit == "0") {
+            if (letterlist[0] == "A2"){
+                letterlist.length = 0
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"="
+            }else{
+                document.getElementById("query").innerHTML = document.getElementById("query").innerHTML+"0"
             }
         }
     }
