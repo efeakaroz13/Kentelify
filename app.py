c71f826da0a0bf1ec90ffcb5554c78d2a0406cb8
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-04 17:07:34 +0300

    playlist create and listing available playlists on profile

--- app.py
+++ app.py
@@ -1,79 +1,145 @@
-from flask import Flask,render_template,request,redirect,make_response
-from flask_cors import CORS 
-from kauth import kauth 
+from os import abort
+from tabnanny import check
+from unicodedata import name
+from flask import Flask, render_template, request, redirect, make_response, abort
+from flask_cors import CORS
+from kauth import kauth
 from cryptography.fernet import Fernet
+from playlistmanager import manager
 
-
-key=b'D71kHIq7Wsyrjd30avvyzrS7BTT74lAXBB5y6mllnsQ='
+key = b'D71kHIq7Wsyrjd30avvyzrS7BTT74lAXBB5y6mllnsQ='
 fernet = Fernet(key)
+
+
 def encrypt(text):
-	encodedtext = fernet.encrypt(text.encode())
-	return encodedtext.decode()
+    encodedtext = fernet.encrypt(text.encode())
+    return encodedtext.decode()
+
+
 def decrypt(text):
-	encodedtext = fernet.decrypt(text.encode())
-	return encodedtext.decode()
+    encodedtext = fernet.decrypt(text.encode())
+    return encodedtext.decode()
 
 
 auth = kauth("Kentelify")
 app = Flask(__name__)
 CORS(app)
+
+
 @app.route("/")
 def index():
-	username = request.cookies.get("username")
-	password = request.cookies.get("password")
-	if username == None or username.strip() =="":
+    username = request.cookies.get("username")
+    password = request.cookies.get("password")
+    if username == None or username.strip() == "":
 
-		return render_template("base.html")
-	else:
-		username = decrypt(username)
-		password = decrypt(request.cookies.get("password"))
-		return render_template("home.html")
+        return render_template("base.html")
+    else:
+        username = decrypt(username)
+        password = decrypt(request.cookies.get("password"))
 
-@app.route("/register",methods=["POST","GET"])
+        outcheck = auth.login(username, password)
+        if outcheck["SCC"] == True:
+            try:
+                myapp = manager(username, password)
+                myplaylists = myapp.playlister()
+                return render_template("home.html",playlists=myplaylists)
+
+            except Exception as e:
+                return str(e)
+
+        else:
+            return render_template("base.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
 def register():
-	msg = request.args.get("msg")
-	if msg!=None:
-		if msg == "noscc":
-			return render_template("register.html",error=True)
+    msg = request.args.get("msg")
+    if msg != None:
+        if msg == "noscc":
+            return render_template("register.html", error=True)
 
-	if request.method == "POST":
-		username =request.form.get("username")
-		password = request.form.get("password")
-		fullname = request.form.get("fullname")
-		response = auth.createuser(username=username,password=password,fullname=fullname)
-		if response["SCC"] == True:
-			return redirect("/login?msg=scc")
-		if response["SCC"] == False:
-			return redirect("/register?msg=noscc")
-	return render_template("register.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        fullname = request.form.get("fullname")
+        response = auth.createuser(
+            username=username, password=password, fullname=fullname)
+        if response["SCC"] == True:
+            return redirect("/login?msg=scc")
+        if response["SCC"] == False:
+            return redirect("/register?msg=noscc")
+    return render_template("register.html")
 
-@app.route("/login",methods=["POST","GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-	if request.method == "POST":
+    if request.method == "POST":
 
-		username = request.form.get("username")
-		password = request.form.get("password")
-		check = auth.login(username,password)
-		if check["SCC"] == True:
-			response = make_response(redirect("/"))
-			usernameencrypted = encrypt(username)
-			response.set_cookie("username",usernameencrypted)
-			passwordencrypted = encrypt(password)
-			response.set_cookie("password",passwordencrypted)
-			return response
-		if check["SCC"] == False:
-			return redirect("/login?msg=noscc")
-	msg = request.args.get("msg")
-	if msg !=None:
-		if msg == "scc":
-			return render_template("login.html",scc=True)
-		else:
-			pass
-		if msg == "noscc":
-			return render_template("login.html",error=True)
-	else:
-		pass 
-	return render_template("login.html")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        check = auth.login(username, password)
+        if check["SCC"] == True:
+            response = make_response(redirect("/"))
+            usernameencrypted = encrypt(username)
+            response.set_cookie("username", usernameencrypted)
+            passwordencrypted = encrypt(password)
+            response.set_cookie("password", passwordencrypted)
+            return response
+        if check["SCC"] == False:
+            return redirect("/login?msg=noscc")
+    msg = request.args.get("msg")
+    if msg != None:
+        if msg == "scc":
+            return render_template("login.html", scc=True)
+        else:
+            pass
+        if msg == "noscc":
+            return render_template("login.html", error=True)
+    else:
+        pass
+    return render_template("login.html")
+
+
+@app.route("/create_playlist", methods=["POST", "GET"])
+def createplaylist():
+    username = request.cookies.get("username")
+    password = request.cookies.get("password")
+    try:
+        username = decrypt(username)
+        password = decrypt(password)
+        mymanager = manager(username, password)
+
+        if request.method == "POST":
+            playlistname = request.form.get("playlistname")
+            description = request.form.get("description")
+            mymanager.create_playlist(playlistname, description)
+            return redirect("/")
+        if request.method == "GET":
+            return render_template("create.html")
+
+    except:
+        return redirect("/")
+
+
+@app.route("/playlist/<playlist_id>")
+def playlist(playlist_id):
+
+    username = request.cookies.get("username")
+    if username == None:
+        return abort(403)
+    else:
+        username = decrypt(username)
+        password = decrypt(request.cookies.get("password"))
+        try:
+            mymanager = manager(username, password)
+            checkout = mymanager.viewplaylist(playlistid=playlist_id)
+            if checkout["SCC"] == True:
+                return checkout
+            else:
+                return"404"
+        except Exception as e:
+            return str(e)
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ playlistmanager.py
@@ -0,0 +1,88 @@
+import os
+import random
+from tabnanny import check
+
+from flask import redirect
+import kauth
+import time 
+import json 
+from cryptography.fernet import Fernet
+auth = kauth.kauth("Kentelify")
+letterlist = ["I","K","Z","A","T","r","a","M","l"]
+key = b'D71kHIq7Wsyrjd30avvyzrS7BTT74lAXBB5y6mllnsQ='
+fernet = Fernet(key)
+
+
+def encrypt(text):
+	encodedtext = fernet.encrypt(text.encode())
+	return encodedtext.decode()
+
+
+def decrypt(text):
+	encodedtext = fernet.decrypt(text.encode())
+	return encodedtext.decode()
+
+class manager:
+  def __init__(self,username,password):
+    self.username=username
+    self.password = password
+    outcheck = auth.login(username,password)
+    if outcheck["SCC"] == True:
+      self.loggedin = True
+    else:
+      raise ValueError("Username or password is not correct")
+
+  def create_playlist(self,playlistname,description):
+    firstletter = random.choice(letterlist)
+    secondletter = random.choice(letterlist)
+    thirdletter = random.choice(letterlist)
+    strout = f"{firstletter}{secondletter}{thirdletter}{random.randint(1,123432955854)}"
+    try:
+      os.listdir("playlists")
+    except:
+      os.system("mkdir playlists")
+    playlistfile = open(f"playlists/{self.username}.{strout}","w")
+    data = json.dumps({"username":self.username,"description":description,"title":playlistname,"time":time.ctime(time.time()),"musics":[]},indent=4)
+    encrypteddata = encrypt(str(data))
+    playlistfile.write(encrypteddata)
+
+    return {"SCC":True,"id":strout}
+  def viewplaylist(self,playlistid):
+    authchecker = auth.login(self.username,self.password) 
+    if authchecker["SCC"] == True:
+
+      allplaylists = os.listdir("playlists")
+      outputlist = []
+      for p in allplaylists:
+        try:
+          playlistfile = p.split(playlistid)[1]
+
+          outputlist.insert(0,p)
+        except:
+          pass 
+      if len(outputlist) == 0:
+        return {"SCC":False}
+      else:
+        myfile = json.loads(decrypt(open("playlists/"+outputlist[0],"r").read()))
+        myfile["SCC"]=True
+        return myfile
+    else:
+      raise ValueError("Username or password is not correct")
+  
+  def playlister(self):
+    checkoutput = auth.login(self.username,self.password)
+    if checkoutput["SCC"] == True:
+      allplaylists = os.listdir("playlists")
+      myplaylists = []
+      for p in allplaylists:
+        try:
+          id_ = p.split(self.username)[1]
+          myplaylists.append(self.viewplaylist(id_))
+        except:
+          pass
+
+      
+      return myplaylists
+    else:
+      return redirect("/")
+    
